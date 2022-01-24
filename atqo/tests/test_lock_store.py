f158def9da24e5ad2c9a5618d9fe111d5697fc18
Endre Márk Borza <endremborza@gmail.com>
2022-01-24 21:28:09 +0100

    init in test

--- atqo/tests/test_lock_store.py
+++ atqo/tests/test_lock_store.py
@@ -3,7 +3,7 @@ from atqo.lock_stores import FileLockStore
 
 def test_lock_store():
 
-    lock_store = FileLockStore()
+    lock_store = FileLockStore("")
 
     lock = lock_store.acquire("__some__")
 
