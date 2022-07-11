c9dbeff782b087fe60f88e6d995984a8795a6732
Endre Márk Borza <endremborza@gmail.com>
2022-07-11 19:14:27 +0200

    partial_cls

--- atqo/simplified_functions.py
+++ atqo/simplified_functions.py
@@ -6,11 +6,11 @@ from .bases import ActorBase
 from .core import Scheduler, SchedulerTask
 from .distributed_apis import DEFAULT_MULTI_API
 from .resource_handling import Capability, CapabilitySet
-from .utils import partial_wrap
+from .utils import partial_cls
 
 _RES = "CPU"
 _CAP = Capability({_RES: 1})
-_Task = partial_wrap(SchedulerTask, requirements=[_CAP])
+_Task = partial_cls(SchedulerTask, requirements=[_CAP])
 
 
 class BatchProd:
@@ -35,7 +35,7 @@ def get_simp_scheduler(n, fun, dist_sys, verbose, max_runs) -> Scheduler:
     ActWrap.restart_after = max_runs
 
     return Scheduler(
-        actor_dict={CapabilitySet([_CAP]): partial_wrap(ActWrap, fun=fun)},
+        actor_dict={CapabilitySet([_CAP]): partial_cls(ActWrap, fun=fun)},
         resource_limits={_RES: n},
         distributed_system=dist_sys,
         verbose=verbose,
--- atqo/utils.py
+++ atqo/utils.py
@@ -1,4 +1,4 @@
-from functools import partial, update_wrapper
+from functools import partial, partialmethod
 from operator import add, sub
 
 
@@ -6,8 +6,12 @@ def dic_merge(d1, d2, _def, fun):
     return {k: fun(d1.get(k, _def), d2.get(k, _def)) for k in [*d1, *d2]}
 
 
-def partial_wrap(func, *args, **kwargs):
-    return update_wrapper(partial(func, *args, **kwargs), func)
+def partial_cls(cls: type, *args, **kwargs):
+    _dict = {
+        k: (v if k != "__init__" else partialmethod(v, *args, **kwargs))
+        for k, v in cls.__dict__.items()
+    }
+    return type(cls.__name__, cls.__bases__, _dict)
 
 
 sumdict = partial(dic_merge, _def=0, fun=add)
