d1164fdcf15315d9f78bf75bc28ad3f7a6e09f43
Endre Márk Borza <endremborza@gmail.com>
2022-08-25 18:56:24 +0200

    bump v

--- atqo/__init__.py
+++ atqo/__init__.py
@@ -7,4 +7,4 @@ from .exceptions import UnexpectedCapabilities  # noqa: F401
 from .resource_handling import Capability, CapabilitySet  # noqa: F401
 from .simplified_functions import parallel_map  # noqa: F401
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
