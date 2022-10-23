cdb4483df69e78d9384f2805d0117e91a4aa8cf4
Endre Márk Borza <endremborza@gmail.com>
2022-10-23 23:27:17 +0200

    try loop thing

--- atqo/core.py
+++ atqo/core.py
@@ -62,7 +62,11 @@ class Scheduler:
         self._active_async_tasks = set()
         self._task_queues: Dict[CapabilitySet, TaskQueue] = {}
         self._loop = _get_loop_of_daemon()
-        self._reorg_lock = asyncio.Lock()
+        try:
+            self._reorg_lock = asyncio.Lock(loop=self._loop)
+        except TypeError:
+            # 3.10 removed the loop kw
+            self._reorg_lock = asyncio.Lock()
 
         self._verbose = verbose
         self._dist_api: DistAPIBase = get_dist_api(distributed_system)()
